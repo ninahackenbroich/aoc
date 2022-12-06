0bf8526d7942201d258067d923f0cb9fba13fe65
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2022-12-06 18:05:04 +0100

    sorted

--- 5/day_five.rb
+++ 5/day_five.rb
@@ -40,3 +40,25 @@ new_stack.each do |k,v|
 end
 
 p tops.join
+
+
+
+# stacks = 
+# [
+#     "                [V]     [C]     [M]",
+#     "[V]     [J]     [N]     [H]     [V]",
+#     "[R] [F] [N]     [W]     [Z]     [N]",
+#     "[H] [R] [D]     [Q] [M] [L]     [B]",
+#     "[B] [C] [H] [V] [R] [C] [G]     [R]",
+#     "[G] [G] [F] [S] [D] [H] [B] [R] [S]",
+#     "[D] [N] [S] [D] [H] [G] [J] [J] [G]",
+#     "[W] [J] [L] [J] [S] [P] [F] [S] [L]",
+#     " 1   2   3   4   5   6   7   8   9 "
+# ]
+
+# hash = {}
+
+# stacks.map(&:chars).transpose.each do |line|
+#     next if line.last == " "
+#     hash[line.last] = line.reverse.reject{ |x| x == " "}
+# end
